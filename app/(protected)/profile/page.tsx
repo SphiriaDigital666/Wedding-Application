4961334ff610bb909254d175c3c73158bc023b4d
pasan1227 <pasanratnayake@gmail.com>
2024-01-29 16:40:55 +0530

    Family details section added

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -1,5 +1,6 @@
 import AboutMe from './components/about-me';
 import BasicDetails from './components/basic-details';
+import FamilyDetails from './components/family-details';
 import LocationDetails from './components/location-details';
 import MainDetails from './components/main-details';
 import PhotosCarousel from './components/photos-carousel';
@@ -16,6 +17,7 @@ const DashboardPage = ({ user }) => {
       <ReligionDetails user={user} />
       <LocationDetails user={user} />
       <ProfessionalDetails user={user} />
+      <FamilyDetails user={user} />
     </>
   );
 };
