e238a222cf053060e8a6e05be3f194fa363c5aae
pasan1227 <pasanratnayake@gmail.com>
2024-01-29 16:32:20 +0530

    Location details section added

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -1,5 +1,6 @@
 import AboutMe from './components/about-me';
 import BasicDetails from './components/basic-details';
+import LocationDetails from './components/location-details';
 import MainDetails from './components/main-details';
 import PhotosCarousel from './components/photos-carousel';
 import ReligionDetails from './components/religion-details';
@@ -12,6 +13,7 @@ const DashboardPage = ({ user }) => {
       <AboutMe />
       <BasicDetails user={user} />
       <ReligionDetails user={user} />
+      <LocationDetails user={user} />
     </>
   );
 };
