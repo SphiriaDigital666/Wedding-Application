5cf71b01addbb792a9870100e209460b5493f3b8
diduladishan <diduladishan14@gmail.com>
2024-02-05 15:21:20 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -17,37 +17,37 @@ export default function Home() {
         <Carousel />
 
         <div className="absolute bottom-[60px] left-[40px]">
-          <p className="text-[#fff] text-[48px] sofia font-bold border-b-4 border-[#D9D9D9]">
+          <p className="text-[#fff] text-[48px] 2xl:text-[48px] xl:text-[40px] lg:text-[36px] md:text-[20px] sofia font-bold border-b-4 border-[#D9D9D9] w-max hidden md:block">
             The biggest and most trusted<br></br> Matrimony service for Tamils!
           </p>
-          <p className="text-[#fff] text-[27px] font-light mt-2">
+          <p className="text-[#fff] text-[27px] 2xl:text-[27px] xl:text-[23px] lg:text-[20.5px] md:text-[11.5px] font-light mt-2 hidden md:block">
             Now find matches based on your hobbies & interests
           </p>
         </div>
 
-        <div className="absolute bottom-[-80px] right-[200px]">
-          <div className="flex items-center justify-end gap-4 mb-6">
-            <p className="text-[#fff] text-[24px] font-medium">
+        <div className="absolute 2xl:bottom-[-80px] 2xl:right-[200px] xl:bottom-[-80px] xl:right-[200px] lg:bottom-[-80px] lg:right-[100px] md:bottom-[-80px] md:right-[100px] sm:bottom-[-80px] sm:right-[100px]">
+          <div className="flex items-center justify-end gap-4 2xl:mb-6 xl:mb-5 lg:mb-4 md:mb-3 sm:mb-2">
+            <p className="text-[#fff] text-[24px] 2xl:text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] font-medium">
               Already a member ?
             </p>
-            <p className="text-[#fff] text-[24px] font-bold border border-[#fff] rounded-md w-max px-5 cursor-pointer">
+            <p className="text-[#fff] text-[24px] xl:text-[22px] lg:text-[20px] md:text-[18px] sm:text-[16px] font-bold border border-[#fff] rounded-md w-max px-5 cursor-pointer">
               Login
             </p>
           </div>
 
-          <div className="w-[577px] drop-shadow-xl pb-[30px]">
+          <div className="2xl:w-[577px] xl:w-[500px] drop-shadow-xl pb-[30px]">
             <div className="bg-[#5BACE3] rounded-t-lg">
-              <h1 className="text-[#fff] text-[36px] text-center font-bold py-8">
+              <h1 className="text-[#fff] text-[36px] 2xl:text-[36px] xl:text-[30px] lg:text-[26px] md:text-[24px] sm:text-[22px] text-center font-bold 2xl:py-8 xl:py-6 lg:py-4 md:py-2 sm:py-2">
                 Create a Matrimony Profile
               </h1>
             </div>
 
             <div className="bg-[#fff] rounded-b-lg">
-              <h1 className="text-[#445159] text-[30px] text-center font-medium py-4 mx-10 border-b border-[#D9D9D9] ">
+              <h1 className="text-[#445159] text-[30px] 2xl:text-[30px] xl:text-[28px] lg:text-[26px] md:text-[24px] sm:text-[22px] text-center font-medium py-4 mx-10 border-b border-[#D9D9D9] ">
                 Place to find your soulmate
               </h1>
 
-              <div className="mb-8 px-10 mt-12">
+              <div className="mb-8 px-10 mt-12 md:mt-8 sm:mt-6">
                 <Input type="email" placeholder="Name" />
               </div>
 
@@ -55,14 +55,14 @@ export default function Home() {
                 <Input type="email" placeholder="Email" />
               </div>
 
-              <div className="mb-12 px-10">
+              <div className="mb-12 md:mb-8 sm:mb-6 px-10">
                 <Input type="email" placeholder="Password" />
               </div>
 
               <div className="flex items-center justify-center">
-                <div className="bg-[#5BACE3] w-[400px] h-[50px] flex items-center justify-center mb-16">
+                <div className="bg-[#5BACE3] w-full 2xl:h-[50px] sm:h-[40px] flex items-center justify-center  mb-16 sm:mb-12 mx-10">
                   <div className="py-2">
-                    <p className="text-[#FFF] font-medium text-[30px]">
+                    <p className="text-[#FFF] font-medium text-[30px] 2xl:text-[30px] xl:text-[28px] lg:text-[26px] md:text-[24px] sm:text-[22px]">
                       Register
                     </p>
                   </div>
