e3e22d4fe4566c09a7f05f927f830bfa04a7431e
Prathibha Ratnayake <prsthibha@gmail.com>
2024-02-28 20:15:42 +0530

    add chat functionality

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,14 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
