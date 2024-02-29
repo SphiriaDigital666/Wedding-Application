1f388277d9dcbccd86c1729a46d42af56e50d5fc
diduladishan <diduladishan14@gmail.com>
2024-02-29 23:31:32 +0530

    All matches section UI added

--- app/(protected)/chat/layout.tsx
+++ app/(protected)/chat/layout.tsx
@@ -1,9 +1,9 @@
-import { auth } from '@/auth';
-import { getFriendsByUserId } from '@/helpers/get-friends-by-user-id';
-import db from '@/lib/db';
-import Link from 'next/link';
-import SidebarChatList from './_components/sidebar-chat-list';
-import FriendRequestSidebarOptions from './add/_components/friend-request-sidebar-options';
+import { auth } from "@/auth";
+import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
+import db from "@/lib/db";
+import Link from "next/link";
+import SidebarChatList from "./_components/sidebar-chat-list";
+import FriendRequestSidebarOptions from "./add/_components/friend-request-sidebar-options";
 
 export default async function ChatLayout({
   children,
@@ -13,8 +13,8 @@ export default async function ChatLayout({
   const sidebarOptions: SidebarOption[] = [
     {
       id: 1,
-      name: 'Add friend',
-      href: '/chat/add',
+      name: "Add friend",
+      href: "/chat/add",
       // Icon: 'UserPlus',
     },
   ];
@@ -32,7 +32,7 @@ export default async function ChatLayout({
   const unseenRequestCount = unseenRequests.length;
 
   return (
-    <div className='w-full flex h-screen'>
+    <div className="w-full flex h-screen">
       {/* <div className='md:hidden'>
         <MobileChatLayout
           friends={friends}
@@ -42,15 +42,15 @@ export default async function ChatLayout({
         />
       </div> */}
 
-      <div className='hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
+      <div className="hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
         {friendsWithUserData.length > 0 ? (
-          <div className='text-xs font-semibold leading-6 text-gray-400'>
+          <div className="text-xs font-semibold leading-6 text-gray-400">
             Your chats
           </div>
         ) : null}
 
-        <nav className='flex flex-1 flex-col'>
-          <ul role='list' className='flex flex-1 flex-col gap-y-7'>
+        <nav className="flex flex-1 flex-col">
+          <ul role="list" className="flex flex-1 flex-col gap-y-7">
             <li>
               <SidebarChatList
                 sessionId={session?.user.id!}
@@ -58,24 +58,24 @@ export default async function ChatLayout({
               />
             </li>
             <li>
-              <div className='text-xs font-semibold leading-6 text-gray-400'>
+              <div className="text-xs font-semibold leading-6 text-gray-400">
                 Overview
               </div>
 
-              <ul role='list' className='-mx-2 mt-2 space-y-1'>
+              <ul role="list" className="-mx-2 mt-2 space-y-1">
                 {sidebarOptions.map((option) => {
                   // const Icon = Icons[option.Icon];
                   return (
                     <li key={option.id}>
                       <Link
                         href={option.href}
-                        className='text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold'
+                        className="text-gray-700 hover:text-indigo-600 hover:bg-gray-50 group flex gap-3 rounded-md p-2 text-sm leading-6 font-semibold"
                       >
-                        <span className='text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white'>
+                        <span className="text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600 flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white">
                           {/* <Icon className='h-4 w-4' /> */}
                         </span>
 
-                        <span className='truncate'>{option.name}</span>
+                        <span className="truncate">{option.name}</span>
                       </Link>
                     </li>
                   );
@@ -93,7 +93,7 @@ export default async function ChatLayout({
         </nav>
       </div>
 
-      <aside className='max-h-screen container py-16 md:py-12 w-full'>
+      <aside className="max-h-screen container py-16 md:py-12 w-full">
         {children}
       </aside>
     </div>
