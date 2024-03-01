4b3b727e8440f288fda0ab5b5d6a52fdafc65bd9
pasan1227 <pasanratnayake@gmail.com>
2024-03-01 23:31:58 +0530

    Cover photo added

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -8,22 +8,37 @@ import PhotosCarousel from './_components/photos-carousel';
 import ProfessionalDetails from './_components/professional-details';
 import ReligionDetails from './_components/religion-details';
 import { currentUser } from '@/lib/auth';
+import Image from 'next/image';
+import AllMatches from '@/public/allMatches/all-matches-img.png';
 
-const DashboardPage = async() => {
-
-  const sessionUser = await currentUser()
+const DashboardPage = async () => {
+  const sessionUser = await currentUser();
 
   const userProfile = await db.userProfile.findFirst({
-    where:{
+    where: {
       userId: sessionUser?.id,
-    }
-  })
+    },
+  });
 
-  console.log(userProfile)
+  console.log(userProfile);
   return (
     <>
-      <MainDetails profile={userProfile}/>
-      <PhotosCarousel profile={userProfile}/>
+      <div className="relative flex items-center justify-center">
+        <Image
+          src={AllMatches}
+          alt="Main Image"
+          width={1920}
+          height={312}
+          className=""
+        />
+        <div className="absolute text-center">
+          <p className="text-[48px] text-[#fff] font-bold">
+            The biggest and most trusted Matrimony service for Tamils!
+          </p>
+        </div>
+      </div>
+      <MainDetails profile={userProfile} />
+      <PhotosCarousel profile={userProfile} />
       <AboutMe />
       <BasicDetails user={userProfile} />
       <ReligionDetails user={userProfile} />
