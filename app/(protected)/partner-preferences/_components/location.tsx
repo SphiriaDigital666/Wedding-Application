2e03448a55d390b268f69712f7f0bf2d7a27eefa
pasan1227 <pasanratnayake@gmail.com>
2024-03-04 12:30:30 +0530

    Location Preferences styles updated

--- app/(protected)/partner-preferences/_components/location.tsx
+++ app/(protected)/partner-preferences/_components/location.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
-import EditLocation from './edit/edit-location';
+import { Separator } from '@/components/ui/separator';
+import { Pencil } from 'lucide-react';
 import { UserProfile } from '@prisma/client';
 
 interface LocationPreferencesProps {
@@ -8,24 +9,41 @@ interface LocationPreferencesProps {
 
 const LocationPreferences: FC<LocationPreferencesProps> = ({ user }) => {
   return (
-    <div className="justify-between p-5">
-      <div className="flex justify-between">
-        <span className="text-2xl">Location Preferences</span>
-        <EditLocation user={user} />
-      </div>
-      <div className="grid grid-cols-2 justify-between gap-5 mt-4">
-        <div className="flex gap-x-2 items-center">
-          <span>Country: </span>
-          <span className=" text-gray-600">{user?.name || 'Not defined'}</span>
+    <div className="p-5">
+      <span className="text-2xl">Location Preferences</span>
+      <div className="flex flex-col gap-4 mt-5">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span>Country: </span>
+            <span className=" text-gray-600">
+              {user?.country || 'Not defined'}
+            </span>
+          </div>
+          <Pencil />
         </div>
-        <div className="flex gap-x-2 items-center">
-          <span>City: </span>
-          <span className=" text-gray-600">{user?.name || 'Not defined'}</span>
+        <Separator />
+
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span>City: </span>
+            <span className=" text-gray-600">
+              {user?.city || 'Not defined'}
+            </span>
+          </div>
+          <Pencil />
         </div>
-        <div className="flex gap-x-2 items-center">
-          <span>State: </span>
-          <span className=" text-gray-600">{user?.name || 'Not defined'}</span>
+        <Separator />
+
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col">
+            <span>State: </span>
+            <span className=" text-gray-600">
+              {user?.state || 'Not defined'}
+            </span>
+          </div>
+          <Pencil />
         </div>
+        <Separator />
       </div>
     </div>
   );
