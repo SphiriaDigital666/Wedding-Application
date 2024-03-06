68d14c7e14f8232742ede4cbd6a67f8eb7240fd4
Prathibha Ratnayake <prsthibha@gmail.com>
2024-03-06 20:18:52 +0530

    add emji picker for chat messages

--- app/(protected)/chat/[chatId]/_components/chat-input.tsx
+++ app/(protected)/chat/[chatId]/_components/chat-input.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { sendMessage } from "@/actions/messages/send-message";
-import { Button } from "@/components/ui/button";
-import { User } from "@prisma/client";
-import { FC, startTransition, useRef, useState } from "react";
-import TextareaAutosize from "react-textarea-autosize";
-import { MdOutlineSend } from "react-icons/md";
-import { Camera, Plus } from "lucide-react";
-import { useModal } from "@/hooks/useModalStore";
+import { sendMessage } from '@/actions/messages/send-message';
+import EmojiPicker from '@/components/emoji-picker';
+import { useModal } from '@/hooks/useModalStore';
+import { User } from '@prisma/client';
+import { Camera } from 'lucide-react';
+import { FC, startTransition, useRef, useState } from 'react';
+import { MdOutlineSend } from 'react-icons/md';
+import TextareaAutosize from 'react-textarea-autosize';
 interface ChatInputProps {
   chatPartner: User;
   chatId: string;
@@ -15,16 +15,15 @@ interface ChatInputProps {
 
 const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
   const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [input, setInput] = useState<string>("");
+  const [input, setInput] = useState<string>('');
 
   const { onOpen } = useModal();
 
-  const handleSendMessage = (text: string, chatId: string) => {
+  const handleSendMessage = (chatId: string, text: string) => {
     if (!input) return;
     startTransition(() => {
-      sendMessage(text, chatId).then((data) => {
-        setInput("");
+      sendMessage(chatId, text, '').then((data) => {
+        setInput('');
         textareaRef.current?.focus();
       });
     });
@@ -33,27 +32,27 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
   return (
     <>
       <div className='border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0 flex'>
-      <button
-        type='button'
-        onClick={() => onOpen('messageFile', {chatId})}
-        className=' h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center'
-      >
-        <Camera className='text-white dark:text-[#313338]' />
-      </button>
+        <button
+          type='button'
+          onClick={() => onOpen('messageFile', { chatId })}
+          className=' h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p-1 flex items-center justify-center'
+        >
+          <Camera className='text-white dark:text-[#313338]' />
+        </button>
         <div className='relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 '>
           <TextareaAutosize
             ref={textareaRef}
             onKeyDown={(e) => {
               if (e.key === 'Enter' && !e.shiftKey) {
                 e.preventDefault();
-                handleSendMessage(input, chatId);
+                handleSendMessage(chatId, input);
               }
             }}
             rows={1}
             value={input}
             onChange={(e) => setInput(e.target.value)}
             placeholder={`Message ${chatPartner.name}`}
-            className='block w-full resize-none  bg-transparent text-gray-900 placeholder:text-gray-400 sm:py-1.5 sm:text-sm sm:leading-6 py-3 px-3'
+            className='block w-full h-full resize-none  bg-transparent text-gray-900 placeholder:text-gray-400 sm:py-1.5 sm:text-sm sm:leading-6 py-3 px-3'
           />
 
           <div
@@ -68,19 +67,18 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
 
           <div className='absolute right-0 bottom-0 flex justify-between py-2 pl-3 pr-2'>
             <div className='flex items-center'>
-              {/* <Button
-              onClick={() => handleSendMessage(input, chatId)}
-              type="submit"
-            >
-              Post 
-            </Button> */}
               <MdOutlineSend
-                onClick={() => handleSendMessage(input, chatId)}
+                onClick={() => handleSendMessage(chatId, input)}
                 type='submit'
                 className='text-[24px] text-[#adadad] cursor-pointer'
               />
             </div>
           </div>
+        </div>
+        <div className=''>
+          <EmojiPicker
+            onChange={(emoji: string) => setInput((prev) => prev + emoji)}
+          />
         </div>
       </div>
     </>
--- app/(protected)/chat/[chatId]/_components/messages.tsx
+++ app/(protected)/chat/[chatId]/_components/messages.tsx
@@ -61,65 +61,6 @@ const Messages: FC<MessagesProps> = ({
           messages[index - 1]?.senderId === messages[index].senderId;
 
         return (
-          //       <div
-          //         className='chat-message'
-          //         key={`${message.id}-${message}`}
-          //       >
-          //         <div
-          //           className={cn('flex items-end', {
-          //             'justify-end': isCurrentUser,
-          //           })}
-          //         >
-          //           <div
-          //             className={cn(
-          //               'flex flex-col space-y-2 text-base max-w-xs mx-2',
-          //               {
-          //                 'order-1 items-end': isCurrentUser,
-          //                 'order-2 items-start': !isCurrentUser,
-          //               }
-          //             )}
-          //           >
-          //             <span
-          //               className={cn('px-4 py-2 rounded-lg inline-block', {
-          //                 'bg-indigo-600 text-white': isCurrentUser,
-          //                 'bg-gray-200 text-gray-900': !isCurrentUser,
-          //                 'rounded-br-none':
-          //                   !hasNextMessageFromSameUser && isCurrentUser,
-          //                 'rounded-bl-none':
-          //                   !hasNextMessageFromSameUser && !isCurrentUser,
-          //               })}
-          //             >
-          //               {message.text}{' '}
-          //               <span className='ml-2 text-xs text-gray-400'>
-          //                 {formatTimestamp(message.createdAt as unknown as number)}
-          //               </span>
-          //             </span>
-          //           </div>
-
-          //           <div
-          //             className={cn('relative w-6 h-6', {
-          //               'order-2': isCurrentUser,
-          //               'order-1': !isCurrentUser,
-          //               invisible: hasNextMessageFromSameUser,
-          //             })}
-          //           >
-          //             <Image
-          //               fill
-          //               src={
-          //                 isCurrentUser
-          //                   ? (sessionImg as string)
-          //                   : chatPartner.image || ''
-          //               }
-          //               alt='Profile picture'
-          //               referrerPolicy='no-referrer'
-          //               className='rounded-full'
-          //             />
-          //           </div>
-          //         </div>
-          //       </div>
-          //     );
-          //   })}
-          // </div>
           <div className='chat-message' key={`${message.id}-${message}`}>
             <div
               className={cn('flex items-end', {
@@ -135,21 +76,24 @@ const Messages: FC<MessagesProps> = ({
                   }
                 )}
               >
-                <ImageModal
-                  src={message.fileUrl}
-                  isOpen={imageModalOpen}
-                  onClose={() => setImageModalOpen(false)}
-                />
                 {/* Displaying image if fileUrl is available */}
                 {message.fileUrl ? (
-                  <Image
-                    src={message.fileUrl}
-                    alt='Attached Image'
-                    className='rounded-md max-w-xs cursor-pointer'
-                    width={200}
-                    height={200}
-                    onClick={() => setImageModalOpen(true)}
-                  />
+                  <>
+                    <ImageModal
+                      src={message.fileUrl}
+                      isOpen={imageModalOpen}
+                      onClose={() => setImageModalOpen(false)}
+                    />
+
+                    <Image
+                      src={message.fileUrl}
+                      alt='Attached Image'
+                      className='rounded-md max-w-xs cursor-pointer'
+                      width={200}
+                      height={200}
+                      onClick={() => setImageModalOpen(true)}
+                    />
+                  </>
                 ) : (
                   <span
                     className={cn('px-4 py-2 rounded-lg inline-block', {
