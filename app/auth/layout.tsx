2be2b7accce0c0386efdc00cf020d35c5f25706e
diduladishan <diduladishan14@gmail.com>
2024-03-07 17:33:57 +0530

    login section UI update

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,9 +1,20 @@
-const AuthLayout = ({children}: {children: React.ReactNode}) => {
+import Image from 'next/image';
+import BG_IMG from '@/public/login/login_bg_img.png';
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center">
-      {children}
+    <div className='h-screen flex items-center justify-center bg-[#c94c8a]'>
+      <div className='grid grid-cols-2'>
+        <div>
+          <div className=''>
+            <Image src={BG_IMG} alt='Main Image' className='rounded-l-lg' />
+          </div>
+        </div>
+
+        <div className='bg-[#ffb9db] rounded-r-lg'>{children}</div>
+      </div>
     </div>
   );
-}
- 
+};
+
 export default AuthLayout;
