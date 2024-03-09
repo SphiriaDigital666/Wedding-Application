e50cd119553c69fd7719fcad199612d952b5e56c
Prathibha Ratnayake <prsthibha@gmail.com>
2024-03-09 19:46:28 +0530

    loading style fix

--- app/(protected)/matches/loading.tsx
+++ app/(protected)/matches/loading.tsx
@@ -1,36 +1,122 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
 const LoadingState = () => {
-  return [1, 2, 3, 4].map((i: number) => (
-    <div className='flex gap-x-8 mb-3 w-[400px]' key={i}>
-      <div>
-        <Skeleton className='h-[200px] w-[200px]' />
-      </div>
-      <div>
-        <Skeleton className='h-[10px] w-[150px]' />
-        <div className='flex mt-2 gap-x-3 mb-5'>
-          <Skeleton className='h-[10px] w-[100px]' />
-          <Skeleton className='h-[10px] w-[100px]' />
+  return (
+    <div className='flex-col'>
+      <div className='flex gap-x-8 mb-3 w-[400px]'>
+        <div>
+          <Skeleton className='h-[200px] w-[200px]' />
         </div>
-        {[1, 2, 3].map((j) => (
-          <ul className='flex gap-4' key={j}>
-            <Skeleton className='w-[70px] h-[10px] mb-3' />
-            <Skeleton className='w-[80px] h-[10px]' />
-            <Skeleton className='w-[80px] h-[10px]' />
+        <div>
+          <Skeleton className='h-[10px] w-[150px]' />
+          <div className='flex mt-2 gap-x-3 mb-5'>
+            <Skeleton className='h-[10px] w-[100px]' />
+            <Skeleton className='h-[10px] w-[100px]' />
+          </div>
+          {[1, 2, 3].map((j) => (
+            <ul className='flex gap-4' key={j}>
+              <Skeleton className='w-[70px] h-[10px] mb-3' />
+              <Skeleton className='w-[80px] h-[10px]' />
+              <Skeleton className='w-[80px] h-[10px]' />
+              <Skeleton className='w-[70px] h-[10px]' />
+            </ul>
+          ))}
+          <div className='mt-6 space-y-1'>
+            <Skeleton className='w-[100px] h-[10px]' />
             <Skeleton className='w-[70px] h-[10px]' />
-          </ul>
-        ))}
-        <div className='mt-6 space-y-1'>
-          <Skeleton className='w-[100px] h-[10px]' />
-          <Skeleton className='w-[70px] h-[10px]' />
+          </div>
+          <div className='flex items-center mt-4 space-x-3'>
+            <Skeleton className='w-[70px] h-[20px]' />
+            <Skeleton className='w-[70px] h-[20px]' />
+          </div>
         </div>
-        <div className='flex items-center mt-4 space-x-3'>
-          <Skeleton className='w-[70px] h-[20px]' />
-          <Skeleton className='w-[70px] h-[20px]' />
+      </div>
+      <div className='flex gap-x-8 mb-3 w-[400px]'>
+        <div>
+          <Skeleton className='h-[200px] w-[200px]' />
+        </div>
+        <div>
+          <Skeleton className='h-[10px] w-[150px]' />
+          <div className='flex mt-2 gap-x-3 mb-5'>
+            <Skeleton className='h-[10px] w-[100px]' />
+            <Skeleton className='h-[10px] w-[100px]' />
+          </div>
+          {[1, 2, 3].map((j) => (
+            <ul className='flex gap-4' key={j}>
+              <Skeleton className='w-[70px] h-[10px] mb-3' />
+              <Skeleton className='w-[80px] h-[10px]' />
+              <Skeleton className='w-[80px] h-[10px]' />
+              <Skeleton className='w-[70px] h-[10px]' />
+            </ul>
+          ))}
+          <div className='mt-6 space-y-1'>
+            <Skeleton className='w-[100px] h-[10px]' />
+            <Skeleton className='w-[70px] h-[10px]' />
+          </div>
+          <div className='flex items-center mt-4 space-x-3'>
+            <Skeleton className='w-[70px] h-[20px]' />
+            <Skeleton className='w-[70px] h-[20px]' />
+          </div>
+        </div>
+      </div>
+      <div className='flex gap-x-8 mb-3 w-[400px]'>
+        <div>
+          <Skeleton className='h-[200px] w-[200px]' />
+        </div>
+        <div>
+          <Skeleton className='h-[10px] w-[150px]' />
+          <div className='flex mt-2 gap-x-3 mb-5'>
+            <Skeleton className='h-[10px] w-[100px]' />
+            <Skeleton className='h-[10px] w-[100px]' />
+          </div>
+          {[1, 2, 3].map((j) => (
+            <ul className='flex gap-4' key={j}>
+              <Skeleton className='w-[70px] h-[10px] mb-3' />
+              <Skeleton className='w-[80px] h-[10px]' />
+              <Skeleton className='w-[80px] h-[10px]' />
+              <Skeleton className='w-[70px] h-[10px]' />
+            </ul>
+          ))}
+          <div className='mt-6 space-y-1'>
+            <Skeleton className='w-[100px] h-[10px]' />
+            <Skeleton className='w-[70px] h-[10px]' />
+          </div>
+          <div className='flex items-center mt-4 space-x-3'>
+            <Skeleton className='w-[70px] h-[20px]' />
+            <Skeleton className='w-[70px] h-[20px]' />
+          </div>
+        </div>
+      </div>
+      <div className='flex gap-x-8 mb-3 w-[400px]'>
+        <div>
+          <Skeleton className='h-[200px] w-[200px]' />
+        </div>
+        <div>
+          <Skeleton className='h-[10px] w-[150px]' />
+          <div className='flex mt-2 gap-x-3 mb-5'>
+            <Skeleton className='h-[10px] w-[100px]' />
+            <Skeleton className='h-[10px] w-[100px]' />
+          </div>
+          {[1, 2, 3].map((j) => (
+            <ul className='flex gap-4' key={j}>
+              <Skeleton className='w-[70px] h-[10px] mb-3' />
+              <Skeleton className='w-[80px] h-[10px]' />
+              <Skeleton className='w-[80px] h-[10px]' />
+              <Skeleton className='w-[70px] h-[10px]' />
+            </ul>
+          ))}
+          <div className='mt-6 space-y-1'>
+            <Skeleton className='w-[100px] h-[10px]' />
+            <Skeleton className='w-[70px] h-[10px]' />
+          </div>
+          <div className='flex items-center mt-4 space-x-3'>
+            <Skeleton className='w-[70px] h-[20px]' />
+            <Skeleton className='w-[70px] h-[20px]' />
+          </div>
         </div>
       </div>
     </div>
-  ));
+  );
 };
 
 export default LoadingState;
