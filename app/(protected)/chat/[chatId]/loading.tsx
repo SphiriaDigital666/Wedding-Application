581a2bb0052b7d81287a4ea40273a4714ae30ef1
Prathibha Ratnayake <prsthibha@gmail.com>
2024-03-09 18:45:09 +0530

    bugs and styling fixes

--- app/(protected)/chat/[chatId]/loading.tsx
+++ app/(protected)/chat/[chatId]/loading.tsx
@@ -3,7 +3,7 @@ import Skeleton from 'react-loading-skeleton';
 
 interface loadingProps {}
 
-const loading: import { FC } from 'react';oadingProps> = ({}) => {
+const loading: FC<loadingProps> = ({}) => {
   return (
     <div className='flex flex-col h-full items-center'>
       <Skeleton className='mb-4' height={40} width={400} />
--- app/(protected)/search/page.tsx
+++ app/(protected)/search/page.tsx
@@ -1,9 +1,9 @@
-"use client";
-import React, { useState } from "react";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import Criteria from "./_components/_by-criteria/criteria";
-import ProfileId from "./_components/_profile-id/profile-id";
-import SavedSearch from "./_components/_saved-search/saved-search";
+'use client';
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { useState } from 'react';
+import Criteria from './_components/_by-criteria/criteria';
+import ProfileId from './_components/_profile-id/profile-id';
+import SavedSearch from './_components/_saved-search/saved-search';
 
 enum STEPS {
   By_Criteria,
@@ -12,13 +12,13 @@ enum STEPS {
 }
 
 const stepLabels = {
-  [STEPS.By_Criteria]: "By Criteria",
-  [STEPS.Profile_Id]: "By Profile Id",
-  [STEPS.Saved_Search]: "Saved Search",
+  [STEPS.By_Criteria]: 'By Criteria',
+  [STEPS.Profile_Id]: 'By Profile Id',
+  [STEPS.Saved_Search]: 'Saved Search',
 };
 
 const SearchPage = () => {
-  const [currentStep, setCurrentStep] = useState<String>("By Criteria");
+  const [currentStep, setCurrentStep] = useState<String>('By Criteria');
 
   const changeState = (step: String) => {
     setCurrentStep(step);
@@ -29,31 +29,33 @@ const SearchPage = () => {
   };
 
   return (
-    <Card className="w-[90%] m-auto">
-      <CardHeader>
-        <div className="text-md text-start flex gap-3">
-          {["By Criteria", "By Profile Id", "Saved Search"].map((step) => (
-            <p
-              key={step}
-              className={`p-0 ${
-                currentStep === step ? "font-bold" : "font-thin"
-              }`}
-              onClick={() => handleStepClick(step)}
-            >
-              {step}
-            </p>
-          ))}
+    <div className='container'>
+      <Card className='w-[90%] m-auto'>
+        <CardHeader>
+          <div className='text-md text-start flex gap-3 hover:cursor-pointer'>
+            {['By Criteria', 'By Profile Id'].map((step) => (
+              <p
+                key={step}
+                className={`p-0 ${
+                  currentStep === step ? 'font-bold' : 'font-thin'
+                }`}
+                onClick={() => handleStepClick(step)}
+              >
+                {step}
+              </p>
+            ))}
+          </div>
+        </CardHeader>
+        <div className='w-full flex justify-center'>
+          <hr className='w-[97%]' />
         </div>
-      </CardHeader>
-      <div className="w-full flex justify-center">
-        <hr className="w-[97%]" />
-      </div>
-      <CardContent className="px-0">
-        {currentStep === "By Criteria" && <Criteria />}
-        {currentStep === "By Profile Id" && <ProfileId />}
-        {currentStep === "Saved Search" && <SavedSearch />}
-      </CardContent>
-    </Card>
+        <CardContent className='px-0'>
+          {currentStep === 'By Criteria' && <Criteria />}
+          {currentStep === 'By Profile Id' && <ProfileId />}
+          {currentStep === 'Saved Search' && <SavedSearch />}
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
