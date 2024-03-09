78d56d32f0a5cba45207cb588272f9797b361c98
Prathibha Ratnayake <prsthibha@gmail.com>
2024-03-09 19:01:03 +0530

    nearby matches bug fix

--- app/(protected)/matches/newly-joined/page.tsx
+++ app/(protected)/matches/newly-joined/page.tsx
@@ -1,13 +1,30 @@
 import db from '@/lib/db';
 import { MatchCard } from '../_component/match-card';
+import { auth } from '@/auth';
 
 export default async function NewMatchesPage() {
+  const session = await auth();
   const currentDate = new Date();
   const thirtyDaysAgo = new Date();
   thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
 
+
+  const currentUserId = session?.user.id;
+
+  const currentUserProfile = await db.userProfile.findFirst({
+    where: {
+      userId: currentUserId,
+    },
+    include: {
+      preference: true,
+    },
+  });
+
   const matches = await db.userProfile.findMany({
     where: {
+      id: {
+        not: currentUserProfile?.id,
+      },
       createdAt: {
         gte: thirtyDaysAgo,
         lte: currentDate,
