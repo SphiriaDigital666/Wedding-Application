a9717ea4ffee529e6c7c7dfc82d93eea1cea0e06
Prathibha Ratnayake <prsthibha@gmail.com>
2024-03-09 18:48:30 +0530

    add newly joined matches section

--- app/(protected)/matches/newly-joined/page.tsx
+++ app/(protected)/matches/newly-joined/page.tsx
@@ -2,7 +2,19 @@ import db from '@/lib/db';
 import { MatchCard } from '../_component/match-card';
 
 export default async function NewMatchesPage() {
-  const matches = await db.userProfile.findMany({});
+  const currentDate = new Date();
+  const thirtyDaysAgo = new Date();
+  thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+  const matches = await db.userProfile.findMany({
+    where: {
+      createdAt: {
+        gte: thirtyDaysAgo,
+        lte: currentDate,
+      },
+    },
+  });
+
   return (
     <div className='col-span-6 '>
       {matches.map((match) => (
