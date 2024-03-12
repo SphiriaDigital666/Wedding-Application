ef720696ce961778d21c545af8d3d212e20d2924
Prathibha Ratnayake <prsthibha@gmail.com>
2024-03-12 13:09:51 +0530

    bug fix

--- helpers/convert-height.ts
+++ helpers/convert-height.ts
@@ -1,12 +1,12 @@
-export function convertHeight(height:number) {
-    // Split the height into feet and inches
-    const [feet, inches] = height.toString().split('.');
-    
-    // Convert feet and inches to inches
-    const totalInches = parseInt(feet) * 12 + parseInt(inches);
-    
-    // Convert inches to centimeters
-    const centimeters = totalInches * 2.54;
-    
-    return `${feet} Ft ${inches} In / ${centimeters.toFixed(2)} Cms`;
+export function convertHeight(height: number = 0) {
+  // Split the height into feet and inches
+  const [feet, inches] = height?.toString().split('.') || ['0', '0'];
+
+  // Convert feet and inches to inches
+  const totalInches = parseInt(feet) * 12 + parseInt(inches);
+
+  // Convert inches to centimeters
+  const centimeters = totalInches * 2.54;
+
+  return `${feet} Ft ${inches} In / ${centimeters.toFixed(2)} Cms`;
 }
